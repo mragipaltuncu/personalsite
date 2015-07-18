64aa3c1a6c6c3ac8a779522554d7c61fb25fb815
mragipaltuncu <mehmetragipaltuncu@gmai.com>
2015-07-18 18:18:23 +0300

    pagination to index.html

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
-from blog import models
+from . import models
 
-class BlogIndexView(generic.TemplateView):
+class BlogIndexView(generic.ListView):
     model = models.Entry
     template_name = "index.html"
+    paginate_by = 2
 
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super(BlogIndexView, self).get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['entry'] = models.Entry.objects.order_by('created').last()
-        return context
 
 def index(request):
     entry = models.Entry.objects.order_by('created').last()
