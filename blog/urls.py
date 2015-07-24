c08126f331663a6c39224adcdf9992aaa5b57e3a
mragipaltuncu <mehmetragipaltuncu@gmai.com>
2015-07-24 19:36:36 +0300

    created archiveview and templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,14 +5,13 @@ urlpatterns = [
     url(r'^$',
         views.BlogIndexView.as_view(),
         name="index"),
-    url(r'^blog/all_posts/$',
-        views.EntryListView.as_view(),
-        name="entry_list"),
     url(r'^about/$',
         views.AboutView.as_view(),
         name="about"),
     url(r'^blog/entry_detail/(?P<slug>[-\w]+)/$',
         views.EntryDetailView.as_view(),
         name="entry_detail"),
-
+    url(r'^blog/archive/$',
+        views.EntryArchiveView.as_view(),
+        name="entry_archive"),
     ]
--- blog/views.py
+++ blog/views.py
@@ -19,8 +19,22 @@ class EntryDetailView(generic.DetailView):
     model = models.Entry
     template_name = "entry_detail.html"
 
+class EntryArchiveView(generic.dates.ArchiveIndexView):
+    model = models.Entry
+    date_field = "created"
+    template_name = "entry_archive.html"
+    def years(self):
+        date_time_list = models.Entry.objects.all().datetimes('created','year')
+        years = [date.year for date in date_time_list ]
+        return years
 
-class TagFilterView(generic.ListView,slug):
+"""
+
+experimenting to create a view which would take a tag from the url and filter objects to
+show them in a listview
+
+class TagFilterView(generic.ListView,slug=None):
     entries = models.Entry.objects.all()
-    queryset=[entry for entry in entries if entry.tags.filter(tag=slug)]
+    queryset=[entry for entry in entries if entry.tags.filter(tag=None)]
     template_name = "tag_filter.html"
+"""
